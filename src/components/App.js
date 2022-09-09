5f5837a75eb0d87b93feb18959cfe1855ec7df51
just-jhorge <sarpongk247@gmail.com>
2022-09-09 01:45:44 +0000

    files updated

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+const App = () => {
+    return (
+        <div className="h-screen w-screen">
+            <div className="h-full w-full">
+                <Header />
+            </div>
+        </div>
+    );
+};
+
+export default App;
